71eabf19ab4de9d50918d0023658ca16ad4d315c
Kalib65 <160874746+Kalib65@users.noreply.github.com>
2024-05-01 14:35:03 +0000

    javascript traning

---
+++ 01_basic/first_basic.js
@@ -0,0 +1,15 @@
+const account_Id = 14455
+let account_Email = "kalibsamson@gmail.com"
+var account_Paswoord = "12345"
+account_City = "karachi"
+
+// account_Id = 2  //not allowed const ke andr any waly value change nhi hogi
+
+account_Email = "hello@gmail.com"
+account_Paswoord = "6548"
+account_City = "multan"
+
+console.log(account_Id);
+console.table([account_Id, account_Email, account_Paswoord, account_City])
+
+// prefre nhi hai ve var ko use kiya jaye kunke ye masla karta hai block scope mai or functional scope mai
